86d6da98b64c8f67b2409820f3f97b1ce771edba
Arthur Marques <dev.arthur.mrp@gmail.com>
2022-10-21 14:08:01 -0300

    Review Code

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 // Const
 const app = express()
+const port = 3000
 // Express preferences
 app.use(express.json())
 // Post method
-app.post('/',(req, res) => {
-    console.log("Requisição feita por " + req.ip + "  " )
+app.post('/', (req, res) => {
+    console.log("Requisição feita por " + req.ip + "  ")
     console.log(req.body)
     res.send("Work!")
 });
 // Port listen
-app.listen(3000)
+app.listen(port, () => {
+    console.log(`The server started on port ${port}`)
+})
