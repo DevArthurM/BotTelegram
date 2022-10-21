203f21ecf192b3a26e0e6e3add09f616a2613a51
Arthur Marques <dev.arthur.mrp@gmail.com>
2022-10-21 12:17:09 -0300

    FirstCommit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+// Const
+const app = express()
+// Express preferences
+app.use(express.json())
+// Post method
+app.post('/',(req, res) => {
+    console.log("Requisição feita por " + req.ip + "  " )
+    console.log(req.body)
+    res.send()
+});
+// Port listen
+app.listen(3000)
